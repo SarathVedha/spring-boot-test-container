e075f0a401c1b59943e5b25a942b5868d70de758
Vedha <sarath1242000@gmail.com>
2024-03-16 20:57:28 +0530

    Spring Boot Testing

--- src/test/java/com/vedha/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/vedha/repository/EmployeeRepositoryTests.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // To Use MySql Or Oracle
 public class EmployeeRepositoryTests {
 
     @Autowired
